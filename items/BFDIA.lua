9c74787856f6af9777c450c0eaeda1d86195a006
GhostSalt <mhanna756@c2ken.net>
2025-03-19 23:56:40 +0000

    -Working on the BFDIA abilities!

--- items/BFDIA.lua
+++ items/BFDIA.lua
@@ -17,18 +17,24 @@ SMODS.Atlas {
 
 SMODS.current_mod.optional_features = { cardareas = { unscored = true } }
 
+SMODS.Sound({
+	key = "yellow_face",
+	path = "bfdi_yellow_face.ogg",
+  replace = true
+})
+
 SMODS.Joker {
   key = 'bomby',
   loc_txt = {
     name = 'Bomby',
     text = {
-      "Earn {C:money}$#1#{} for each",
-      "{C:blue}hand{} remaining",
-      "at the end of",
-      "the {C:attention}round{}"
+      "If played hand is a",
+      "{C:attention}High Card{}, destroys all",
+      "cards held in hand",
+      "{C:red,E:2}self destructs{}"
     }
   },
-  config = { extra = { is_contestant = true, given_money = 2 } },
+  config = { extra = { is_contestant = true } },
   rarity = 2,
   atlas = 'BFDIA',
   pos = { x = 0, y = 0 },
@@ -45,27 +51,27 @@ SMODS.Joker {
 }
 
 SMODS.Joker {
-  key = 'puffball',
+  key = 'yellowface',
   loc_txt = {
-    name = 'Puffball',
+    name = 'Yellow Face',
     text = {
-      "Earn {C:money}$#1#{} for each",
-      "{C:blue}hand{} remaining",
-      "at the end of",
-      "the {C:attention}round{}"
+      "Gives {C:money}$#1#{} when a",
+      "consumable is sold"
     }
   },
   config = { extra = { is_contestant = true, given_money = 2 } },
   rarity = 2,
   atlas = 'BFDIA',
+  pos = { x = 1, y = 1 },
   cost = 7,
   blueprint_compat = false,
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.extra.given_money } }
   end,
-  calc_dollar_bonus = function(self, card)
-    if G.GAME.current_round.hands_left > 0 then
-      return card.ability.extra.given_money * G.GAME.current_round.hands_left
+  calculate = function(self, card, context)
+    if context.selling_card and context.card.config.center.set ~= "Joker" then
+      play_sound("bfdi_yellow_face", 1, 0.5)
+      return { dollars = card.ability.extra.given_money }
     end
   end
 }
